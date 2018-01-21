dfdcd0eef08804220af5ab2800f490655530431a
Yav <yavieron@hotmail.es>
2018-01-21 18:37:16 +0100

    Añadidos a la clase Track el campo playCount y dos métodos para reinicializar e incrementar en una unidad el valor de playCount

--- Track.java
+++ Track.java
@@ -13,7 +13,9 @@ public class Track
     private String title;
     // Where the track is stored.
     private String filename;
-    
+    // Contabiliza el número de reproducciones de las canciones.
+    private int playCount;
+
     /**
      * Constructor for objects of class Track.
      * @param artist The track's artist.
@@ -23,8 +25,9 @@ public class Track
     public Track(String artist, String title, String filename)
     {
         setDetails(artist, title, filename);
+        playCount = 0;
     }
-    
+
     /**
      * Constructor for objects of class Track.
      * It is assumed that the file name cannot be
@@ -34,8 +37,9 @@ public class Track
     public Track(String filename)
     {
         setDetails("unknown", "unknown", filename);
+        playCount = 0;
     }
-    
+
     /**
      * Return the artist.
      * @return The artist.
@@ -44,7 +48,7 @@ public class Track
     {
         return artist;
     }
-    
+
     /**
      * Return the title.
      * @return The title.
@@ -53,7 +57,7 @@ public class Track
     {
         return title;
     }
-    
+
     /**
      * Return the file name.
      * @return The file name.
@@ -62,7 +66,7 @@ public class Track
     {
         return filename;
     }
-        
+
     /**
      * Return details of the track: artist, title and file name.
      * @return The track's details.
@@ -71,7 +75,7 @@ public class Track
     {
         return artist + ": " + title + "  (file: " + filename + ")";
     }
-    
+
     /**
      * Set details of the track.
      * @param artist The track's artist.
@@ -84,5 +88,18 @@ public class Track
         this.title = title;
         this.filename = filename;
     }
-    
+
+    /**
+     * Incrementa 1 unidad el valor de playCount.
+     */
+    public void incrementarUnaUnidadPlayCount() {
+        playCount += 1;
+    }
+
+    /**
+     * Reinicializa a 0 el valor de playCount.
+     */
+    public void reinicializarPlayCount() {
+        playCount = 0;
+    }
 }
